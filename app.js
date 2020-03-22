dad78acb48dae0d08d77b6fe64cb7477d8656804
Nilutpal <47278844+Nilutpal2020@users.noreply.github.com>
2020-03-22 18:25:49 +0530

    Initial upload - 22/03/2020

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express"),
+    app = express(),
+    ejs = require("ejs"),
+    bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set("view engine", "ejs");
+
+let notices = [{
+        name: "Day Order",
+        description: "The day order of today is 3"
+    },
+    {
+        name: "Presentation",
+        description: "The upcoming presentation will be held on the 20th of this month."
+    }
+];
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/notices", (req, res) => {
+    res.render("notices", {
+        //campgrounds: campgrounds
+        notices: notices
+    });
+});
+
+app.post("/notices", (req, res) => {
+    //get data from form
+    let name = req.body.name;
+    let description = req.body.description;
+    let newNotice = {
+        name: name,
+        description: description
+    };
+    notices.push(newNotice);
+    //redirect back to campgrounds page
+    res.redirect("/notices");
+});
+
+app.get("/notices/new", (req, res) => {
+    res.render("new");
+});
+
+app.listen(3000, () => {
+    console.log("Server has started!");
+});
